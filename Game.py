be75441acb6f72d61d7602c9f10f496bd03087aa
Paweł Szczygieł <ps371816@students.mimuw.edu.pl>
2016-11-18 20:28:11 +0100

    guess fix

--- Game.py
+++ Game.py
@@ -23,18 +23,20 @@ class Game:
     def guess(self, word):
         current_state_of_guessing = list("_" * len(word))
         guessed_letters = 0
+        guesses_left = self.guesses
         print("".join(current_state_of_guessing))
-        for i in range(self.guesses):
+        while guesses_left > 0:
             players_guess = utils.read_guess()
             if len(players_guess) > 1:
                 if players_guess == "".join(word):
                     self.correct_guessed_word(word)
                     return 1
-
-                self.wrong_guess_message(i, current_state_of_guessing)
+                guesses_left -= 1
+                self.wrong_guess_message(guesses_left, current_state_of_guessing)
             else:
                 if players_guess not in word:
-                    self.wrong_guess_message(i, current_state_of_guessing)
+                    guesses_left -= 1
+                    self.wrong_guess_message(guesses_left, current_state_of_guessing)
                 else:
                     guess_occurrences = [a for a, x in enumerate(word) if x == players_guess]
                     guessed_letters += len(guess_occurrences)
@@ -45,22 +47,22 @@ class Game:
 
                     for j in guess_occurrences:
                         current_state_of_guessing[j] = players_guess
-                    self.correct_guessed_letter(i, current_state_of_guessing)
+                    self.correct_guessed_letter(guesses_left, current_state_of_guessing)
 
         print("".join(["Hasłem było: ", "".join(word)]))
         return 0
 
-    def wrong_guess_message(self, i, current_state_of_guessing):
+    def wrong_guess_message(self, guesses_left, current_state_of_guessing):
         print("Wrong guess")
         print("".join(current_state_of_guessing))
         print("Guesses left:")
-        print(self.guesses - i - 1)
+        print(guesses_left)
 
-    def correct_guessed_letter(self, i, current_message_of_guessing):
+    def correct_guessed_letter(self, guesses_left, current_message_of_guessing):
         print("Correct guess")
         print("".join(current_message_of_guessing))
         print("Guesses left:")
-        print(self.guesses - i - 1)
+        print(guesses_left)
 
     def correct_guessed_word(self, word):
         print("You guessed word")
